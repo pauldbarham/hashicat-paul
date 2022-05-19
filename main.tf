e71af7f7e00ed8be3055c9ccce0a29c591712e46
pauldbarham <103535616+pauldbarham@users.noreply.github.com>
2022-05-19 17:07:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_eip_association" "hashicat" {
 resource "aws_instance" "hashicat" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.hashicat-paul.key_name
+  key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
